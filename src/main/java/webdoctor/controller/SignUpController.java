08821d1782cb2f305319339601e28c52909c560d
jiaozi1995 <zhou482@purdue.edu>
2016-10-11 16:59:15 -0400

    modified

--- src/main/java/webdoctor/controller/SignUpController.java
+++ src/main/java/webdoctor/controller/SignUpController.java
@@ -1,8 +1,10 @@
 package webdoctor.controller;
 
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import webdoctor.jooq.tables.pojos.User;
 
 /**
  * Created by IAN on 2016/10/10.
@@ -11,7 +13,8 @@ import org.springframework.web.bind.annotation.RestController;
 public class SignUpController {
 
     @RequestMapping(path="/SignUp", method= RequestMethod.PUT)
-    public int SignUp() {
+    public int SignUp(@RequestBody User user) {
+
         return 0;
     }
 
