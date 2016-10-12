bdd1595ade17f6acdf61b9630398294b4f8126af
jiaozi1995 <zhou482@purdue.edu>
2016-10-12 14:25:32 -0400

    implemented signup

--- src/main/java/webdoctor/controller/SignUpController.java
+++ src/main/java/webdoctor/controller/SignUpController.java
@@ -1,27 +1,32 @@
 package webdoctor.controller;
 
-import org.omg.CORBA.Request;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import webdoctor.jooq.tables.pojos.User;
-
+import webdoctor.service.LogIn;
 /**
  * Created by IAN on 2016/10/10.
  */
 @Controller
 public class SignUpController {
 
+    @Autowired
+    LogIn login;
+
     @RequestMapping(path="/SignUpPage", method = RequestMethod.GET)
     public String SignUp() {
         return "signupPage.html";
     }
 
-    @RequestMapping(path="/SignUp", method= RequestMethod.POST)
-    public @ResponseBody int SignUp(@RequestBody User user) {
-        System.out.println("Success");
-        System.out.println(user);
+    @RequestMapping(path="/SignUp", method= RequestMethod.GET)
+    public @ResponseBody int SignUpCheck(@RequestBody User user) {
+        return login.checkValid(user);
+    }
 
-        return 1;
+    @RequestMapping(path="/SignUp", method = RequestMethod.POST)
+    public @ResponseBody int SignUp(@RequestBody User user) {
+        return login.signin(user);
     }
 
     @RequestMapping(path="/Login", method=RequestMethod.GET)
---
+++ src/main/java/webdoctor/service/LogIn.java
@@ -0,0 +1,36 @@
+package webdoctor.service;
+
+import org.jooq.DSLContext;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import webdoctor.jooq.tables.pojos.User;
+import static webdoctor.jooq.Tables.USER;
+
+/**
+ * Created by IAN on 2016/10/11.
+ */
+
+@Service
+public class LogIn {
+    //need to add dependency
+    private final DSLContext create;
+
+    @Autowired
+    public LogIn(DSLContext dslContext) {
+        this.create = dslContext;
+    }
+
+    public int checkValid(User user) {
+        User checkedUser = create.select().from(USER).where(USER.USERNAME.equal(user.getUsername())).fetchOneInto(User.class);
+        if (checkedUser == null) {
+            return 1;
+        }
+        else {
+            return 0;
+        }
+    }
+
+    public int signin(User user) {
+        return create.insertInto(USER, USER.USERNAME, USER.PASSWORD, USER.EMAIL, USER.AUTHORITY).values(user.getUsername(), user.getPassword(), user.getEmail(), "0").execute();
+    }
+}
