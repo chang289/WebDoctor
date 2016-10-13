0bc950facd6b74bc90c5d5e05779a4aa3de2ab14
jiaozi1995 <zhou482@purdue.edu>
2016-10-13 16:40:52 -0400

    modified

--- src/main/java/webdoctor/controller/SearchController.java
+++ src/main/java/webdoctor/controller/SearchController.java
@@ -1,5 +1,6 @@
 package webdoctor.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import webdoctor.jooq.tables.pojos.Disease;
 
@@ -8,8 +9,13 @@ import webdoctor.jooq.tables.pojos.Disease;
  */
 
 
-@RestController
+@Controller
 public class SearchController {
+
+    @RequestMapping(path="/DepartmentPage", method = RequestMethod.GET)
+    public String departmentPage() {
+        return "departmentPage.html";
+    }
 
     @RequestMapping(path = "/searchByName", method= RequestMethod.GET)
     public @ResponseBody String searchByName(@RequestBody Disease disease) {
@@ -22,9 +28,10 @@ public class SearchController {
         return "searchByTags";
     }
 
-    @RequestMapping(path = "/symptomsByDepartment", method=RequestMethod.GET)
+    @RequestMapping(path = "/symptomsByDepartment", method=RequestMethod.POST)
     public @ResponseBody String searchByDepartment(@RequestBody String department) {
         System.out.println(department);
         return "success";
     }
+
 }
