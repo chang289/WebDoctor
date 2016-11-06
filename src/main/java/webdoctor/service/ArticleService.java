f79d4e2618f304b286ae70bcfec76c4e9a0901eb
Yibih <wu689@purdue.edu>
2016-11-06 14:36:25 -0500

    table changed

---
+++ src/main/java/webdoctor/service/ArticleService.java
@@ -0,0 +1,40 @@
+package webdoctor.service;
+
+
+import org.jooq.DSLContext;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import webdoctor.jooq.tables.pojos.Article;
+import webdoctor.jooq.tables.pojos.User;
+import static webdoctor.jooq.Tables.USER;
+import static webdoctor.jooq.Tables.ARTICLE;
+import static webdoctor.jooq.Tables.DISEASE_ARTICLE;
+import static webdoctor.jooq.Tables.USER_FAVOURITEARTICLE;
+import static webdoctor.jooq.Tables.COMMENT;
+
+
+/**
+ * Created by Yibih on 2016/10/30.
+ */
+
+@Service
+public class ArticleService {
+
+    private final DSLContext create;
+
+    @Autowired
+    public ArticleService(DSLContext dslContext) {
+        this.create = dslContext;
+    }
+
+
+
+    public int postArticle(Article article) {
+        return create.insertInto(ARTICLE,ARTICLE.AUTHOR_ID,ARTICLE.TITLE,ARTICLE.TIME_STAMP,ARTICLE.CONTENT)
+                .values(article.getAuthorId(), article.getTitle(), article.getTimeStamp(),article.getContent()).execute();
+
+    }
+
+
+
+}
