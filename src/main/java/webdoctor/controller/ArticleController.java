a5156924c9b8f49ae83df6245ef3a601181c8bde
jiaozi1995 <zhou482@purdue.edu>
2016-11-10 22:01:24 -0500

    modified

--- src/main/java/webdoctor/controller/ArticleController.java
+++ src/main/java/webdoctor/controller/ArticleController.java
@@ -4,6 +4,7 @@ import com.google.gson.Gson;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
+import webdoctor.jooq.tables.pojos.Disease;
 import webdoctor.service.ArticleService;
 import webdoctor.service.Authorization;
 import webdoctor.jooq.tables.pojos.User;
@@ -17,15 +18,16 @@ import webdoctor.jooq.tables.pojos.Article;
 @Controller
 public class ArticleController {
     @Autowired
-    ArticleService article;
+    ArticleService articleService;
     Comment comment;
 
     @RequestMapping(path = "/ArticlePostPage", method = RequestMethod.GET)
     public String ArticlePostPage() {return "articlePost.html";}
 
     @RequestMapping(path = "/ArticleCreate", method = RequestMethod.POST)
-    public @ResponseBody int CreateDescription(@RequestBody Article article){
+    public @ResponseBody int createArticle(@RequestBody Article article){
         System.out.println(article);
+        articleService.postArticle(article);
         return 1;
     }
 
@@ -37,5 +39,9 @@ public class ArticleController {
     @RequestMapping(path = "/ArticlePage", method = RequestMethod.GET)
     public String ArticlePage() {return "article.html";}
 
-
+    @RequestMapping(path = "/SendDisease", method = RequestMethod.POST)
+    public @ResponseBody String sendDisease(@RequestBody Disease disease) {
+        System.out.println("list: "+articleService.getRelatedArticle(disease));
+        return "";
+    }
 }
